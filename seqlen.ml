19f7e3077fcab0a774450498498412f33596cbca
Erkki Seppälä <flux@modeemi.fi>
2013-10-13 10:18:52 +0300

    SeqLen: output all data, not just the 'best'

--- seqlen.ml
+++ seqlen.ml
@@ -17,24 +17,35 @@ let seqlen common source =
   let data = Array.of_enum (floats_of source) in
   let data_a = Array.init (Array.length data / 2) (fun i -> data.(i * 2)) in
   let data_b = Array.init (Array.length data / 2) (fun i -> data.(i * 2 + 1)) in
+  let tryK = [1; 2; 3; 4; 5; 6] in
   let find_centroids label data =
-    let centroids = K.bestmeans [1; 2; 3; 4; 5; 6] data in
-    Array.sort compare centroids;
-    Array.iteri
-      (fun idx centroid ->
-	Printf.printf "%s %f " label centroid;
-	let centroid_data = Array.filter (fun value -> K.cluster_of' centroids value = idx) data in
-	Printf.printf "%d %f %f %f %f\n" 
-	  (Array.length centroid_data) (St.mean centroid_data) (St.stddev centroid_data)
-	  (Array.fold_left min centroid_data.(0) centroid_data) (Array.fold_left max centroid_data.(0) centroid_data) 
-	(* Array.iter  *)
-	(*   (fun value -> *)
-	(*     Printf.printf " %d" (int_of_float value) *)
-	(*   ) *)
-	(*   centroid_data; *)
-	(* Printf.printf "\n"; *)
+    let clusters = 
+      List.map 
+	(fun k -> 
+	  let centroids = K.kmeans k data in
+	  Array.sort compare centroids;
+	  (k, centroids))
+	tryK
+    in
+    List.iteri
+      (fun i (k, centroids) ->
+	Array.iteri
+	  (fun idx centroid ->
+	    let centroid_data = Array.filter (fun value -> K.cluster_of' centroids value = idx) data in
+	    Printf.printf "%d %s %f %d %f %f %f %f\n" 
+	      k label centroid
+	      (Array.length centroid_data) (St.mean centroid_data) (St.stddev centroid_data)
+	      (Array.fold_left min centroid_data.(0) centroid_data) (Array.fold_left max centroid_data.(0) centroid_data) 
+      (* Array.iter  *)
+      (*   (fun value -> *)
+      (*     Printf.printf " %d" (int_of_float value) *)
+      (*   ) *)
+      (*   centroid_data; *)
+      (* Printf.printf "\n"; *)
+	  )
+	  centroids
       )
-      centroids
+      clusters
   in
   find_centroids "1" data_a;
   find_centroids "0" data_b;
